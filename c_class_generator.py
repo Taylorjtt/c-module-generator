1f08990db74f71b8ecd032319355c2d1e11dd40f
John Taylor <john.t.taylor@altec.com>
2022-06-05 17:55:17 -0600

    adding functionality for adding member variables and to automatically generate getters and setters. Also added the ability to declare the class as a static/singleton style class of which there is only one copy

--- c_class_generator.py
+++ c_class_generator.py
@@ -1,5 +1,165 @@
 from datetime import datetime
 
+
+def writeStaticGetterSetterDeclaration(file, variable):
+    strings = variable.split(" ")
+    type = strings[0]
+    name = strings[1]
+
+    file.write("""/**
+* @brief
+* Getter for """+name+"""
+* @return """+name+"""
+*/
+"""+type+""" """+className.upper()+"""_get"""+name[0].upper() + name[1:]+"""();
+    
+""")
+
+    file.write("""/**
+* @brief
+* Setter for """ + name + """
+* @param """+name+""" value to set 
+* @return void
+*/
+void """ + className.upper() + """_set"""+name[0].upper() + name[1:]+"""("""+type+""" """+name+""");
+""")
+
+
+def writeGetterSetterDeclaration(file, variable):
+    strings = variable.split(" ")
+    type = strings[0]
+    name = strings[1]
+
+    file.write("""/**
+* @brief
+* Getter for """ + name + """
+* @param handle to the object to operate on 
+* @return """ + name + """
+*/
+""" + type + """ """ + className.upper() + """_get""" + name[0].upper() + name[1:] + """("""+className+"""Handle handle);
+
+""")
+
+    file.write("""/**
+* @brief
+* Setter for """ + name + """
+* @param handle to the object to operate on 
+* @param """+name+""" value to set 
+* @return void
+*/
+void """ + className.upper() + """_set""" + name[0].upper() + name[1:] + """("""+className+"""Handle handle, """ + type + """ """ + name + """);
+""")
+
+def writeHeader(file):
+    file.write("""/*****************************************************************************
+* Copyright (C) """ + year + """ by Myolyn Inc
+*
+* Redistribution, modification or use of this software in source or binary
+* forms is not permitted. Myolyn is not liable for
+* any misuse of this material.
+*
+*****************************************************************************/
+""")
+
+def writeConstructorImpl(file):
+    file.write(className + """Handle """ + className.upper() + """_Constructor(void *pmemory, const size_t numbytes)
+{
+    """ + className + """Handle handle;
+    """ + className.upper() + """Object *obj;
+
+    if(numbytes < sizeof(""" + className.upper() + """Object))
+    {
+        return ((""" + className + """Handle)NULL);
+    }
+
+    handle = (""" + className + """Handle)pmemory;
+    obj = (""" + className.upper() + """Object *)handle;
+    return handle;
+    }
+    
+    """)
+
+def writeStaticConstructorImpl(file):
+
+    file.write("""void """ + className.upper() + """_Constructor()
+{
+    """+variableName +" = k_malloc(sizeof("""+objectName+"""));
+
+    """+objectName+""" *obj;
+    """+variableName+""" = ("""+handleTypeName+""")"""+variableName+""";
+    obj = ("""+objectName+""" *)"""+variableName+""";
+}
+
+"""
+)
+
+def writeStaticGetterSetterImpl(file,variable):
+    strings = variable.split(" ")
+    type = strings[0]
+    name = strings[1]
+
+    file.write("""/**
+* @brief
+* Getter for """ + name + """
+* @return """ + name + """
+*/
+""" + type + """ """ + className.upper() + """_get""" + name[0].upper() + name[1:] + """()
+{
+    """ + castStaticHandleToObject + """
+    return obj->""" + name + """;
+}
+
+""")
+
+    file.write("""/**
+* @brief
+* Setter for """ + name + """
+* @param """ + name + """ value to set 
+* @return void
+*/
+void """ + className.upper() + """_set""" + name[0].upper() + name[1:] + """(""" + type + """ """ + name + """)
+{
+    """ + castStaticHandleToObject + """
+    obj->""" + name + """ = """ + name + """;
+}
+
+""")
+
+def writeGetterSetterImpl(file,variable):
+    strings = variable.split(" ")
+    type = strings[0]
+    name = strings[1]
+
+    file.write("""/**
+* @brief
+* Getter for """ + name + """
+* @param """ + handleTypeName +""" pointer to class
+* @return """ + name + """
+*/
+""" + type + """ """ + className.upper() + """_get""" + name[0].upper() + name[1:] + """(""" + handleTypeName +""" handle)
+{
+    """ + castHandleToObject + """
+    return obj->""" + name + """;
+}
+
+""")
+
+    file.write("""/**
+* @brief
+* Setter for """ + name + """
+* @param """ + handleTypeName +""" pointer to class
+* @param """ + name + """ value to set 
+* @return void
+*/
+void """ + className.upper() + """_set""" + name[0].upper() + name[1:] + """(""" + handleTypeName +""" handle,""" + type + """ """ + name + """)
+{
+    """ + castHandleToObject + """
+    obj->""" + name + """ = """ + name + """;
+}
+
+""")
+
+
 dateTimeObj = datetime.now()
 
 timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
@@ -7,18 +167,32 @@ timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)")
 year = dateTimeObj.strftime("%Y")
 
 className = input("Enter the name of the C module\n")
-authorName = input("Enter your name")
+authorName = input("Enter your name\n")
+numberOfVariables = input("How many member variables does this module have?\n")
 
-h_file = open(className+".h","w+");
-h_file.write("""/*****************************************************************************
-* Copyright (C) """+year+""" by Myolyn Inc
-*
-* Redistribution, modification or use of this software in source or binary
-* forms is not permitted.Myolyn is not liable for
-* any misuse of this material.
-*
-*****************************************************************************/
-""")
+staticResponse = input("Is this a static/singleton module? (y/n) \n");
+variableName = className[0].lower() + className[1:]
+objectName = className.upper()+"""Object"""
+handleTypeName = className+"Handle"
+castStaticHandleToObject = objectName + " *obj = (" + objectName + " *)" + variableName + ";"
+castHandleToObject = objectName + " *obj = (" + objectName + " *)handle;"
+
+isStatic = False;
+if staticResponse == "y":
+    isStatic = True
+
+
+variables = []
+
+for x in range(int(numberOfVariables)):
+    variables.append(input("Enter next variable (ex uint8_t count)\n"))
+
+
+
+
+h_file = open(className+".h","w+")
+
+writeHeader(h_file)
 
 h_file.write("""/*
  * @file """+className+""".h
@@ -28,8 +202,6 @@ h_file.write("""/*
  * 	@date """+timestampStr+"""
  *
  */
-
-
 """)
 
 h_file.write("""#ifndef SRC_"""+className.upper()+"""_H_
@@ -37,6 +209,9 @@ h_file.write("""#ifndef SRC_"""+className.upper()+"""_H_
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <zephyr.h>
 
 """)
 
@@ -46,54 +221,58 @@ h_file.write("""/**
 */
 typedef struct """+className.upper()+"""_OBJ
 {
+""")
 
+for v in variables:
+    h_file.write(" "+v + ";\n")
+
+h_file.write("""
 }"""+className.upper()+"""Object;
 
 typedef struct """+className.upper()+"""_OBJ *"""+className+"""Handle;
+""")
 
-
+if isStatic:
+    h_file.write("""extern """ + className + """Handle """ + variableName+""";""")
+    h_file.write("""
 
 /**
 * @brief Constructor for """+className+""" object
 *
-* Given a chunk of memory, the size of an """+className.upper()+"""Object this function
-* initializes the memory to be used as an and returns a pointer to the object
+*Initializes the singleton """+className+"""
+* @return void
+*/
+    
+void """+className.upper()+"""_Constructor();
+    
+""")
+    for v in variables:
+        writeStaticGetterSetterDeclaration(h_file,v)
+else:
+
+    h_file.write("""
+
+/**
+* @brief Constructor for """ + className + """ object
 *
-* @param pmemory	The pointer to the memory that this object will use
-* @param numbytes	The expected size of the object
-*
-* @return """+className+"""Handle a pointer to an """+className.upper()+"""Object
+* Initializes the class """ + className + """
+* @return """+className+"""handle
 */
 
 """+className+"""Handle """+className.upper()+"""_Constructor(void *pmemory, const size_t numbytes);
 
-
-
-/**
-* @brief
-*
-*
-* @return void
-*/
-void """+className.upper()+"""_("""+className+"""Handle handle);
-
 """)
-
+    for v in variables:
+        writeGetterSetterDeclaration(h_file,v)
 
 h_file.write("\n#endif")
 h_file.close();
 
 c_file = open(className+".c","w+");
 
-c_file.write("""/*****************************************************************************
-* Copyright (C) """+year+""" by Myolyn Inc
-*
-* Redistribution, modification or use of this software in source or binary
-* forms is not permitted.Myolyn is not liable for
-* any misuse of this material.
-*
-*****************************************************************************/
-""")
+
+writeHeader(c_file)
+
 
 c_file.write("""/*
  * @file """+className+""".c
@@ -106,37 +285,20 @@ c_file.write("""/*
 
 
 #include \""""+className+""".h\"
+\n""")
 
-"""+className+"""Handle """+className.upper()+"""_Constructor(void *pmemory, const size_t numbytes)
-{
-	"""+className+"""Handle handle;
-	"""+className.upper()+"""Object *obj;
+if isStatic:
 
-	if(numbytes < sizeof("""+className.upper()+"""Object))
-	{
-		return (("""+className+"""Handle)NULL);
-	}
+    c_file.write(className+"""Handle """ + className[0].lower() + className[1:]+""";\n\n""")
+    writeStaticConstructorImpl(c_file)
+    for v in variables:
+        writeStaticGetterSetterImpl(c_file,v)
 
-	handle = ("""+className+"""Handle)pmemory;
-	obj = ("""+className.upper()+"""Object *)handle;
-
-
-
-	return handle;
-}
+else:
+    writeConstructorImpl(c_file)
+    for v in variables:
+        writeGetterSetterImpl(c_file,v)
 
 
-
-/**
-* @brief
-*
-*
-* @return void
-*/
-void """+className.upper()+"""_("""+className+"""Handle handle)
-{
-    """+className.upper()+"""Object *obj = ("""+className.upper()+"""Object *)handle;
-}
-
-""")
+c_file.close()
 
